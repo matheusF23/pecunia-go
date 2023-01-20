a5a7ef2a3cf4639ee8e7c30f8a67e429bfdb822c
Matheus Figueiredo <figueiredo.matheus06@gmail.com>
2023-01-19 22:04:33 -0300

    feat: add create user service

---
+++ data/services/user/create-user.go
@@ -0,0 +1,18 @@
+package services
+
+import (
+	"github.com/matheusF23/pecunia-go/data/contracts"
+	"github.com/matheusF23/pecunia-go/data/models"
+)
+
+type CreateUserService struct {
+	userRepository contracts.UserRepository
+}
+
+func (createUserService *CreateUserService) Execute(user models.UserModel) error {
+	err := createUserService.userRepository.CreateUser(user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
