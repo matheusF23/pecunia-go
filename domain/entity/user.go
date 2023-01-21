d54e6115fbf536beddc0d5d153fd298da4074206
Matheus Figueiredo <figueiredo.matheus06@gmail.com>
2023-01-21 18:30:30 -0300

    chore: add create new user function to entity and a method to validade user

--- domain/entity/user.go
+++ domain/entity/user.go
@@ -1,7 +1,40 @@
 package entity
 
+import "errors"
+
 type User struct {
+	ID       string
 	Name     string
 	Email    string
 	Password string
+}
+
+func NewUser(id string, name string, email string, password string) (*User, error) {
+	user := &User{
+		ID:       id,
+		Name:     name,
+		Email:    email,
+		Password: password,
+	}
+	err := user.IsValid()
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (user *User) IsValid() error {
+	if user.ID == "" {
+		return errors.New("invalid id")
+	}
+	if user.Name == "" {
+		return errors.New("invalid name")
+	}
+	if user.Email == "" {
+		return errors.New("invalid email")
+	}
+	if user.Password == "" {
+		return errors.New("invalid password")
+	}
+	return nil
 }
